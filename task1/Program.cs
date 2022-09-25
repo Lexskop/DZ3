f9972a1743a8343edd2b603bd92277f487d6d2bc
lexskop <lexskop@gmail.com>
2022-09-25 20:58:22 +0700

    Решены все задачки, init

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+double userDigit4 = Math.Round(((userNumber % Math.Pow(10, 2)) - (userNumber % Math.Pow(10, 1))) / Math.Pow(10, 1));
+double userDigit5 = Math.Round(userNumber % Math.Pow(10, 1));
+double userDigit1 = Math.Round(userNumber / Math.Pow(10, 4));
+double userDigit2 = Math.Round(((userNumber % Math.Pow(10, 4)) - (userNumber % Math.Pow(10, 3))) / Math.Pow(10, 3));
+if (userDigit2 == userDigit4 && userDigit1 == userDigit5)
+{
+    Console.WriteLine($"Ваше число {userNumber} является палиндромом");
+}
+else 
+{
+    Console.WriteLine($"Ваше число {userNumber} не является палиндромом");
+}
---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.WriteLine("Введите координаты Х1");
+int userCoordinateX1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y1");
+int userCoordinateY1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Z1");
+int userCoordinateZ1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Х2");
+int userCoordinateX2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Y2");
+int userCoordinateY2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты Z2");
+int userCoordinateZ2 = Convert.ToInt32(Console.ReadLine());
+double result = Math.Sqrt(Math.Pow(userCoordinateX2-userCoordinateX1, 2) + Math.Pow(userCoordinateY2-userCoordinateY1, 2) + Math.Pow(userCoordinateZ2-userCoordinateZ1, 2));
+Console.WriteLine($"Ответ: {result}");
